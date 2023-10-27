5c8ae7a74ddaeee97136eb494994c002c3476939
jlvogel <jlv153@gmail.com>
2023-10-27 14:05:22 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -19,11 +19,29 @@
 // 3. If a number is divisible by 5 log "Buzz" instead of the number.
 // 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-for(let i = 1; i <= 100; i++){
-  if(i%3==0){console.log("Fizz")
-    if(i%5==0){console.log("FizzBuzz")}
-  }
-  else if(i%5==0){console.log("Buzz")}
-  else {console.log(i)}
-}
+// for(let i = 1; i <= 100; i++){
+//   if(i%3==0){console.log("Fizz")
+//     if(i%5==0){console.log("FizzBuzz")}
+//   }
+//   else if(i%5==0){console.log("Buzz")}
+//   else {console.log(i)}
+// }
 
+// Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down", "NOT A HOME TOWN"]
+
+plantee[2]++
+// console.log(plantee)
+wolfy[3] = "Gotham City"
+dart[3] = [dart[3], "Hawkins"]
+// console.log(dart)
+
+wolfy.shift()
+// console.log(wolfy)
+const gameboy = wolfy
+gameboy.unshift("Gameboy")
+console.log(gameboy)
