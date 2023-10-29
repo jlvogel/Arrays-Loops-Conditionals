768d7dc227c19c57ee953d7835ca26e4babd16ae
jlvogel <jlv153@gmail.com>
2023-10-29 14:27:27 -0400

    Cleaning up code and output for final submission - work in progress

--- answers.js
+++ answers.js
@@ -1,38 +1,59 @@
+////////////////////////////////////////////////
+//////      Easy Going
+////////////////////////////////////////////////
+
 
 // Write a for loop that will log the numbers 1 through 20.
+console.log(`Write a for loop that will log the numbers 1 through 20.`)
 
-// for(let i =1; i<=20; i++){
-//   console.log(i)
-// }
+for(let i =1; i<=20; i++){
+  console.log(i)
+}
+
+
+////////////////////////////////////////////////
+//////      Get Even
+////////////////////////////////////////////////
 
 // Write a for loop that will log only the even numbers in 0 through 200
 
-// for(let i = 0; i <= 200; i=i+2){
-//   console.log(i)
-// }
+console.log(`
+Write a for loop that will log only the even numbers in 0 through 200`)
+
+for(let i = 0; i <= 200; i=i+2){
+  console.log(i)
+}
 
 
-// Fizz Buzz
+////////////////////////////////////////////////
+//////      Fizz Buzz
+////////////////////////////////////////////////
 
-// 1. Write a javascript application that logs all numbers from 1 - 100.
-// 2. If a number is divisible by 3 log "Fizz" instead of the number.
-// 3. If a number is divisible by 5 log "Buzz" instead of the number.
-// 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+console.log(`
+1. Write a javascript application that logs all numbers from 1 - 100.
+2. If a number is divisible by 3 log "Fizz" instead of the number.
+3. If a number is divisible by 5 log "Buzz" instead of the number.
+4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.`)
 
-// for(let i = 1; i <= 100; i++){
-//   if(i%3==0){console.log("Fizz")
-//     if(i%5==0){console.log("FizzBuzz")}
-//   }
-//   else if(i%5==0){console.log("Buzz")}
-//   else {console.log(i)}
-// }
+for(let i = 1; i <= 100; i++){
+  if(i%3==0){console.log("Fizz")
+    if(i%5==0){console.log("FizzBuzz")}
+  }
+  else if(i%5==0){console.log("Buzz")}
+  else {console.log(i)}
+}
 
-// Wild Wild Life
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// const sharky = ["Sharky", "shark", 20, "Left Coast"]
-// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down", "NOT A HOME TOWN"]
+////////////////////////////////////////////////
+//////      Wild Wild Life
+////////////////////////////////////////////////
+
+// Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down", "NOT A HOME TOWN"]
 
 // plantee[2]++
 // // console.log(plantee)
@@ -201,168 +222,168 @@
 // Return of the Closets
 // Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript. Use this data to answer the following questions.
 
-const kristynsCloset = [
-  "left shoe",
-  "cowboy boots",
-  "right sock",
-  "Per Scholas hoodie",
-  "green pants",
-  "yellow knit hat",
-  "marshmallow peeps"
-];
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "Per Scholas hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
 
-const kristynsClosetCopy = JSON.parse(JSON.stringify(kristynsCloset))
+// const kristynsClosetCopy = JSON.parse(JSON.stringify(kristynsCloset))
 
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-  [
-    // These are Thom's shirts
-    "grey button-up",
-    "dark grey button-up",
-    "light blue button-up",
-    "blue button-up",
-  ],[
-    // These are Thom's pants
-    "grey jeans",
-    "jeans",
-    "PJs"
-  ],[
-    // Thom's accessories
-    "wool mittens",
-    "wool scarf",
-    "raybans"
-  ]
-];
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
 
-const thomsClosetCopy = JSON.parse(JSON.stringify(thomsCloset))
+// const thomsClosetCopy = JSON.parse(JSON.stringify(thomsCloset))
 
-// Alien Attire
-// Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+// // Alien Attire
+// // Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
 
-kristynsShoe = kristynsCloset.shift()
+// kristynsShoe = kristynsCloset.shift()
 
-thomsCloset[2].push(kristynsShoe)
+// thomsCloset[2].push(kristynsShoe)
 
-// console.log(thomsCloset)
+// // console.log(thomsCloset)
 
-// Dress Us Up
-// Modify your code to put together 3 separate outfits for Kristyn and Thom. Put the output in a sentence to tell us what we'll be wearing. Mix and match!
+// // Dress Us Up
+// // Modify your code to put together 3 separate outfits for Kristyn and Thom. Put the output in a sentence to tell us what we'll be wearing. Mix and match!
 
-// for Kristyn:
-// pseudocode
-// since Kristyn lost her left shoe to thom she now has 6 items in her closet.  we can choose 3 seperate outfits for Kristyn consisting of 2 items each.  We can do this by randomly sorting the array and popping the last 2 on the array 3 times for the outfits.
+// // for Kristyn:
+// // pseudocode
+// // since Kristyn lost her left shoe to thom she now has 6 items in her closet.  we can choose 3 seperate outfits for Kristyn consisting of 2 items each.  We can do this by randomly sorting the array and popping the last 2 on the array 3 times for the outfits.
 
-let points = kristynsCloset
+// let points = kristynsCloset
 
-for (let i = points.length -1; i > 0; i--) {
-  let j = Math.floor(Math.random() * i)
-  let k = points[i]
-  points[i] = points[j]
-  points[j] = k
-}
+// for (let i = points.length -1; i > 0; i--) {
+//   let j = Math.floor(Math.random() * i)
+//   let k = points[i]
+//   points[i] = points[j]
+//   points[j] = k
+// }
 
-// console.log(points)
+// // console.log(points)
 
-let kristynsOutfit = []
-let l = points.length
-let n = 0
-for(let i=0; i< l; i+=2){
-  // console.log(n, kristynsCloset)
-  kristynsOutfit[n] = [points.pop(), points.pop()]
-  n++
-}
+// let kristynsOutfit = []
+// let l = points.length
+// let n = 0
+// for(let i=0; i< l; i+=2){
+//   // console.log(n, kristynsCloset)
+//   kristynsOutfit[n] = [points.pop(), points.pop()]
+//   n++
+// }
 
-console.log('kristyn\'s 3 outfits are: ',kristynsOutfit)
+// console.log('kristyn\'s 3 outfits are: ',kristynsOutfit)
 
-// Ok Thoms outfits are calculated a little differently:
+// // Ok Thoms outfits are calculated a little differently:
 
-// we will first shuffle his 3 different categories.  Then we will pop like before.
+// // we will first shuffle his 3 different categories.  Then we will pop like before.
 
-for (let item of thomsCloset) {
-  let points = item
-  for (i = points.length -1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i)
-    let k = points[i]
-    points[i] = points[j]
-    points[j] = k
-  }
-}
+// for (let item of thomsCloset) {
+//   let points = item
+//   for (i = points.length -1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * i)
+//     let k = points[i]
+//     points[i] = points[j]
+//     points[j] = k
+//   }
+// }
 
-// console.log(thomsCloset)
+// // console.log(thomsCloset)
 
-let thomsOutfit = []
+// let thomsOutfit = []
 
-for (let i in thomsCloset){
-  thomsOutfit[i] = []
-  for (let item in thomsCloset) {
+// for (let i in thomsCloset){
+//   thomsOutfit[i] = []
+//   for (let item in thomsCloset) {
     
-      // console.log(i, item)
-      // console.log(thomsCloset[item].pop())
-      thomsOutfit[i].push(thomsCloset[item].pop())
+//       // console.log(i, item)
+//       // console.log(thomsCloset[item].pop())
+//       thomsOutfit[i].push(thomsCloset[item].pop())
     
-  }
-  // console.log(thomsOutfit)
-}
-console.log('thom\'s 3 outfits are: ',thomsOutfit)
+//   }
+//   // console.log(thomsOutfit)
+// }
+// console.log('thom\'s 3 outfits are: ',thomsOutfit)
 
-// Dirty Laundry
+// // Dirty Laundry
 
-// Continue looking at the closet arrays:
+// // Continue looking at the closet arrays:
 
-// 1. Time to do laundry - loop through Kristyn's closet and log the sentence "WHIRR: Now washing (item)" for each item in the array
+// // 1. Time to do laundry - loop through Kristyn's closet and log the sentence "WHIRR: Now washing (item)" for each item in the array
 
-for(item of kristynsCloset) {
-  console.log(`WHIRR: Now washing ${item}`)
-}
+// for(item of kristynsCloset) {
+//   console.log(`WHIRR: Now washing ${item}`)
+// }
 
-// we not that the above for loop does not work.
-// why?  Because we completely popped Kristyn's
-// closet - now there's nothing in it
+// // we not that the above for loop does not work.
+// // why?  Because we completely popped Kristyn's
+// // closet - now there's nothing in it
 
-// what we need to do is make a clean copy of
-// kristynsCloset array back in our code then 
-// loop thru that.
+// // what we need to do is make a clean copy of
+// // kristynsCloset array back in our code then 
+// // loop thru that.
 
-// setting another variable to equal kristynsCloset does not work because it assigns by reference not by value.
+// // setting another variable to equal kristynsCloset does not work because it assigns by reference not by value.
 
 
-// Credit to https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/     article by Yazeed Bzadough - scroll down to point 8. JSON.parse(JSON.stringify(nestedNumbers) for the following:
+// // Credit to https://www.freecodecamp.org/news/how-to-clone-an-array-in-javascript-1d3183468f6a/     article by Yazeed Bzadough - scroll down to point 8. JSON.parse(JSON.stringify(nestedNumbers) for the following:
 
-// JSON.parse turns a string into an object.
+// // JSON.parse turns a string into an object.
 
-// Combining them can turn an object into a string, and then reverse the process to create a brand new data structure.
+// // Combining them can turn an object into a string, and then reverse the process to create a brand new data structure.
 
-// Note: This one safely copies deeply nested objects/arrays!
+// // Note: This one safely copies deeply nested objects/arrays!
 
-// nestedNumbers = [[1], [2]];
-// numbersCopy = JSON.parse(JSON.stringify(nestedNumbers));
+// // nestedNumbers = [[1], [2]];
+// // numbersCopy = JSON.parse(JSON.stringify(nestedNumbers));
 
-// numbersCopy[0].push(300);
-// console.log(nestedNumbers, numbersCopy);
+// // numbersCopy[0].push(300);
+// // console.log(nestedNumbers, numbersCopy);
 
-// [[1], [2]]
-// [[1, 300], [2]]
-// These two arrays are completely separate!
+// // [[1], [2]]
+// // [[1, 300], [2]]
+// // These two arrays are completely separate!
 
-for(item of kristynsClosetCopy) {
-  console.log(`WHIRR: Now washing ${item}`)
-}
+// for(item of kristynsClosetCopy) {
+//   console.log(`WHIRR: Now washing ${item}`)
+// }
 
-// Inventory
+// // Inventory
 
-// 2. Thom wants to do inventory on his closet. Using bracket notation, log the arrays containing all of Thom's shirts, pants, and accessories.
+// // 2. Thom wants to do inventory on his closet. Using bracket notation, log the arrays containing all of Thom's shirts, pants, and accessories.
 
-// console.log(thomsCloset)
+// // console.log(thomsCloset)
 
-// need to make a clean copy of Thom's closet also.  going back in code.
+// // need to make a clean copy of Thom's closet also.  going back in code.
 
-console.log(thomsClosetCopy)
+// console.log(thomsClosetCopy)
 
-n = 0
-for(item of thomsClosetCopy) {
-  n++
-  if(n==1){console.log(`Thom's shirts: `)}
-  else if (n==2){console.log(`Thom's pants: `)}
-  else {console.log(`Thom's accessories: `)}
-  console.log(item)
-}
+// n = 0
+// for(item of thomsClosetCopy) {
+//   n++
+//   if(n==1){console.log(`Thom's shirts: `)}
+//   else if (n==2){console.log(`Thom's pants: `)}
+//   else {console.log(`Thom's accessories: `)}
+//   console.log(item)
+// }
